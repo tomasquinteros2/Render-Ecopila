f16e67d86ef4ae0d19f0ccc01fc8a8c1abc191c7
tomasquinteros <82914165+tomasquinteros2@users.noreply.github.com>
2025-08-08 11:04:15 -0300

    Update ProductoRepository.java

--- microservicio_producto/src/main/java/micro/microservicio_producto/repositories/ProductoRepository.java
+++ microservicio_producto/src/main/java/micro/microservicio_producto/repositories/ProductoRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> , JpaSpecificationExecutor<Producto> {
